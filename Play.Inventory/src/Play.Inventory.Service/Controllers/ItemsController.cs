b7bcf7a2e9180e3540d5c478688cb0981cb2a2e4
Caillin Spain <caillin.spain@tekever.com>
2023-10-26 21:15:19 +0100

    Added inventory service

---
+++ Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+using static Play.Inventory.Services.Dtos;
+
+namespace Play.Inventory.Service.Controllers;
+
+[ApiController]
+[Route("items")]
+public class ItemsController : ControllerBase
+{
+	private readonly IRepository<InventoryItem> _itemsRepository;
+
+	public ItemsController(IRepository<InventoryItem> itemsRepository)
+	{
+		_itemsRepository = itemsRepository;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
+	{
+		if (userId == Guid.Empty)
+		{
+			return BadRequest();
+		}
+
+		IEnumerable<InventoryItemDto> items =
+			(await _itemsRepository.GetAllAsync(item => item.UserId == userId))
+			.Select(item => item.AsDto());
+
+		return Ok(items);
+	}
+
+	[HttpPost]
+	public async Task<ActionResult> PostAsync(GrantItemsDto grantItemDto)
+	{
+		InventoryItem? inventoryItem = await _itemsRepository.GetAsync(item => item.Id == grantItemDto.CatalogItemId);
+
+		if (inventoryItem is null)
+		{
+			inventoryItem = new InventoryItem
+			{
+				CatalogItemId = grantItemDto.CatalogItemId,
+				UserId = grantItemDto.UserId,
+				Quantity = grantItemDto.Quantity,
+				AcquiredDate = DateTimeOffset.UtcNow
+			};
+
+			await _itemsRepository.CreateAsync(inventoryItem);
+		}
+		else
+		{
+			inventoryItem.Quantity = grantItemDto.Quantity;
+			await _itemsRepository.UpdateAsync(inventoryItem);
+		}
+
+		return Ok(inventoryItem);
+	}
+}
---
+++ Play.Inventory/src/Play.Inventory.Service/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Play.Inventory.Service.Entities;
+using static Play.Inventory.Services.Dtos;
+
+namespace Play.Inventory.Service;
+
+public static class Extensions
+{
+	public static InventoryItemDto AsDto(this InventoryItem item)
+	{
+		return new InventoryItemDto(item.CatalogItemId, item.Quantity, item.AcquiredDate);
+	}
+}
